41a3a740e3a4486b8c385feae57b2143552ac1ae
Al Stauffer <Allen Stauffer>
2021-06-22 15:27:17 -0600

    added gets

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import os
 from flask import Flask
 from models import setup_db
 from flask_cors import CORS
+from models import Movies, Actors
+
+
+def get_actors():
+    actors = Actors.query.all()
+    if len(actors) == 0:
+        abort(404)
+    return actors
+
+def get_movies():
+    movies = Movies.query.all()
+    if len(movies) == 0:
+        abort(404)
+    return movies
+
 
 def create_app(test_config=None):
 
@@ -9,16 +24,31 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
+    @app.route('/actors', methods=['GET'])
+    def get_drinks():
+        return jsonify({
+            'success': True,
+            'actors': get_actors()
+        })
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+    @app.route('/movies', methods=['GET'])
+    def get_drinks():
+        return jsonify({
+            'success': True,
+            'movies': get_movies()
+        })
+
+
+    # @app.route('/')
+    # def get_greeting():
+    #     excited = os.environ['EXCITED']
+    #     greeting = "Hello" 
+    #     if excited == 'true': greeting = greeting + "!!!!!"
+    #     return greeting
+
+    # @app.route('/coolkids')
+    # def be_cool():
+    #     return "Be cool, man, be coooool! You're almost a FSND grad!"
 
     return app
 
