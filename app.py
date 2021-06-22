5072a55ee006ba1b9ae9127bbe65c9051b26b56e
Al Stauffer <Allen Stauffer>
2021-06-22 15:37:00 -0600

    testing

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from flask_cors import CORS
 from models import Movies, Actors
 
 
-def get_actors():
-    actors = Actors.query.all()
-    if len(actors) == 0:
-        abort(404)
-    return actors
+# def get_actors():
+#     actors = Actors.query.all()
+#     if len(actors) == 0:
+#         abort(404)
+#     return actors
 
-def get_movies():
-    movies = Movies.query.all()
-    if len(movies) == 0:
-        abort(404)
-    return movies
+# def get_movies():
+#     movies = Movies.query.all()
+#     if len(movies) == 0:
+#         abort(404)
+#     return movies
 
 
 def create_app(test_config=None):
@@ -24,19 +24,19 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    @app.route('/actors', methods=['GET'])
-    def get_drinks():
-        return jsonify({
-            'success': True,
-            'actors': get_actors()
-        })
+    # @app.route('/actors', methods=['GET'])
+    # def get_drinks():
+    #     return jsonify({
+    #         'success': True,
+    #         'actors': get_actors()
+    #     })
 
-    @app.route('/movies', methods=['GET'])
-    def get_drinks():
-        return jsonify({
-            'success': True,
-            'movies': get_movies()
-        })
+    # @app.route('/movies', methods=['GET'])
+    # def get_drinks():
+    #     return jsonify({
+    #         'success': True,
+    #         'movies': get_movies()
+    #     })
 
 
     @app.route('/')
