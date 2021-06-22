c2276cd01d94d7683f7ce562c6b3a9796fb8496f
Al Stauffer <Allen Stauffer>
2021-06-22 14:41:16 -0600

    code

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ import json
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
 DB_USER = os.getenv('DB_USER', 'postgres')  
 DB_NAME = os.getenv('DB_NAME', 'capstone')  
-database_path = "postgresql://{}@{}/{}".format(DB_USER, DB_HOST, DB_NAME)
+defaultPath = "postgresql://{}@{}/{}".format(DB_USER, DB_HOST, DB_NAME): 
+database_path = os.getenv('DATABASE_URL', defaultPath)  
 
 db = SQLAlchemy()
 
@@ -15,7 +16,7 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL: 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
