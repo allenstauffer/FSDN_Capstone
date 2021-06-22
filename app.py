f2903c78a818c1295246bcc6c3e01c20dc39fcfc
Al Stauffer <Allen Stauffer>
2021-06-22 15:33:28 -0600

    update

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def create_app(test_config=None):
         })
 
 
-    # @app.route('/')
-    # def get_greeting():
-    #     excited = os.environ['EXCITED']
-    #     greeting = "Hello" 
-    #     if excited == 'true': greeting = greeting + "!!!!!"
-    #     return greeting
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello" 
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
 
     # @app.route('/coolkids')
     # def be_cool():
