b254473f3fffba64dc9406a1dd8e5150aba11445
Al Stauffer <Allen Stauffer>
2021-06-22 15:19:27 -0600

    added new files

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from datetime import datetime
 
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
 DB_USER = os.getenv('DB_USER', 'postgres')  
@@ -24,22 +25,68 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Person
+Movie
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class Movies(db.Model):  
+  __tablename__ = 'Movies'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+  movies_id = Column('movies_id',Integer, primary_key=True)
+  movies_title = Column('movies_title',String)
+  movies_release_date = db.Column('movies_release_date', db.DateTime)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+  def __init__(self, movies_title="", movies_release_date=datetime.today()):
+    self.movies_title = movies_title
+    self.movies_release_date = movies_release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()    
 
   def format(self):
     return {
       'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+      'movies_title': self.movies_title,
+      'movies_release_date': self.movies_release_date}
+
+'''
+Movie
+Have title and release year
+'''
+class Actors(db.Model):  
+  __tablename__ = 'Actors'
+
+  actors_id = Column('actors_id',Integer, primary_key=True)
+  actors_name = Column('actors_name', String)
+  actors_age = Column('actors_age', Integer)
+  actors_gender = Column('actors_gender', String)
+
+  def __init__(self, actors_name="", actors_age=0, actors_gender=""):
+    self.actors_name = actors_name
+    self.actors_age = actors_age
+    self.actors_gender = actors_gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()    
+
+  def format(self):
+    return {
+      'id': self.id,
+      'actors_name': self.actors_name,
+      'actors_age': self.actors_age,
+      'actors_gender': self.actors_gender}
