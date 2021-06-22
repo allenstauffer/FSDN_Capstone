6523e79cc20e8e83901f1767cf478581d899f7ad
Al Stauffer <Allen Stauffer>
2021-06-22 14:16:43 -0600

    Update

---
+++ models.py
@@ -0,0 +1,44 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+DB_USER = os.getenv('DB_USER', 'postgres')  
+DB_NAME = os.getenv('DB_NAME', 'capstone')  
+database_path = "postgresql://{}@{}/{}".format(DB_USER, DB_HOST, DB_NAME)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'catchphrase': self.catchphrase}
---
+++ setup.sh
@@ -0,0 +1,4 @@
+export DB_HOST = '127.0.0.1:5432'
+export DB_USER = 'postgres'
+export DB_NAME = 'capstone'
+
