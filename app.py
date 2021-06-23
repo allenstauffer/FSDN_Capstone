6bee3b0254222a98438ac7c6af209ab71d89a118
Al Stauffer <Allen Stauffer>
2021-06-23 10:50:45 -0600

    push

--- app.py
+++ app.py
@@ -2,53 +2,137 @@ import os
 from flask import Flask, request, jsonify, abort
 from models import setup_db
 from flask_cors import CORS
-from models import Movies, Actors
-
-
-# def get_actors():
-#     actors = Actors.query.all()
-#     if len(actors) == 0:
-#         abort(404)
-#     return actors
-
-# def get_movies():
-#     movies = Movies.query.all()
-#     if len(movies) == 0:
-#         abort(404)
-#     return movies
-
+from models import Movie, Actor
+import sys
 
 def create_app(test_config=None):
-
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
 
     @app.route('/actors', methods=['GET'])
-    def get_drinks():
+    def get_actors():
+        actors = Actors.query.all()
+        if len(actors) == 0:
+            abort(404)
         return jsonify({
             'success': True,
-            'actors': get_actors()
+            'actors': actors
         })
+
+    @app.route('/actors', methods=['POST'])
+    def post_actors():
+        try:
+            body = request.get_json()
+            actor_name = body.get('name', None)
+            actor_age = body.get('age', None)
+            actor_gender = body.get('gender', None)
+
+            print("actor_name")
+            print(actor_name)
+            print("actor_age")
+            print(actor_age)
+            print("actor_gender")
+            print(actor_gender)
+            if actor_name is None or actor_age is None or actor_gender is None:
+                abort(401)
+            print("here")
+            actor = Actor(
+                actor_name=actor_name,
+                actor_age=actor_age,
+                actor_gender=actor_gender
+            )
+            print("here again")
+            actor.insert()
+            return jsonify({
+                "success": True,
+            }), 200
+        except Exception:
+            print(sys.exc_info())
+            abort(422)  
 
     @app.route('/movies', methods=['GET'])
-    def get_drinks():
+    def get_movies():
+        movies = Movies.query.all()
+        if len(movies) == 0:
+            abort(404)
         return jsonify({
             'success': True,
-            'movies': get_movies()
+            'movies': movies()
         })
 
+    @app.route('/movies', methods=['POST'])
+    def post_movies():
+        try:
+            body = request.get_json()
+            title = body.get('title', None)
+            release_date = body.get('release_date', None)
+            if title is None or release_date is None:
+                abort(401)
+            movie = Movie(
+                title=title,
+                recipe=release_date
+            )
+            drink.insert()
+            returnArray = [drink.long()]
+            return jsonify({
+                'success': True,
+                'drinks': returnArray
+            })
+        except Exception:
+            print(sys.exc_info())
+            abort(422)  
 
-    # @app.route('/')
-    # def get_greeting():
-    #     excited = os.environ['EXCITED']
-    #     greeting = "Hello" 
-    #     if excited == 'true': greeting = greeting + "!!!!!"
-    #     return greeting
 
-    # @app.route('/coolkids')
-    # def be_cool():
-    #     return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello" 
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+
+    # Error Handling
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+            }), 400
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+            }), 404
+
+
+    # @app.errorhandler(AuthError)
+    # def authentification_failed(AuthError):
+    #     return jsonify({
+    #         "success": False,
+    #         "error": AuthError.status_code,
+    #         "message": AuthError.error['description']
+    #         }), AuthError.status_code
+
 
     return app
 
--- models.py
+++ models.py
@@ -28,27 +28,27 @@ def setup_db(app, database_path=database_path):
 Movie
 Have title and release year
 '''
-class Movies(db.Model):  
-  __tablename__ = 'Movies'
+class Movie(db.Model):  
+  __tablename__ = 'movie'
 
-  movies_id = Column('movies_id',Integer, primary_key=True)
-  movies_title = Column('movies_title',String)
-  movies_release_date = db.Column('movies_release_date', db.DateTime)
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  movies_title = Column(String)
+  movies_release_date = db.Column(db.Date)
 
   def __init__(self, movies_title="", movies_release_date=datetime.today()):
     self.movies_title = movies_title
     self.movies_release_date = movies_release_date
 
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
+  def insert(self):
+      db.session.add(self)
+      db.session.commit()
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()
 
-    def update(self):
-        db.session.commit()    
+  def update(self):
+      db.session.commit()    
 
   def format(self):
     return {
@@ -60,33 +60,33 @@ class Movies(db.Model):
 Movie
 Have title and release year
 '''
-class Actors(db.Model):  
-  __tablename__ = 'Actors'
+class Actor(db.Model):  
+  __tablename__ = 'actor'
 
-  actors_id = Column('actors_id',Integer, primary_key=True)
-  actors_name = Column('actors_name', String)
-  actors_age = Column('actors_age', Integer)
-  actors_gender = Column('actors_gender', String)
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  actor_name = Column(String)
+  actor_age = Column(Integer)
+  actor_gender = Column(String)
 
-  def __init__(self, actors_name="", actors_age=0, actors_gender=""):
-    self.actors_name = actors_name
-    self.actors_age = actors_age
-    self.actors_gender = actors_gender
+  def __init__(self, actor_name="", actor_age=0, actor_gender=""):
+    self.actor_name = actor_name
+    self.actor_age = actor_age
+    self.actor_gender = actor_gender
 
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
+  def insert(self):
+      db.session.add(self)
+      db.session.commit()
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()
 
-    def update(self):
-        db.session.commit()    
+  def update(self):
+      db.session.commit()    
 
-  def format(self):
-    return {
-      'id': self.id,
-      'actors_name': self.actors_name,
-      'actors_age': self.actors_age,
-      'actors_gender': self.actors_gender}
+def format(self):
+  return {
+    'id': self.id,
+    'actor_name': self.actor_name,
+    'actor_age': self.actor_age,
+    'actor_gender': self.actor_gender}
